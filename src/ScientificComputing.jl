05676906aa7199e8dd510ce86026c0fa63e39527
AlCap23 <julius.martensen@gmail.com>
2021-06-30 17:25:19 +0200

    Re-move slides

--- src/ScientificComputing.jl
+++ src/ScientificComputing.jl
@@ -4,7 +4,7 @@ using Pluto
 
 # Just for the sake of it
 start_slides() = begin
-    Pluto.run(notebook=joinpath(@__DIR__, "notebook.jl"))
+    Pluto.run(notebook=joinpath(@__DIR__, "..", "slides","notebook.jl"))
 end
 
 export start_slides
