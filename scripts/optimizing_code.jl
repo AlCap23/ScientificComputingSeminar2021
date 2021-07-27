6d2971700f853cf93a7af69ce780bd42880623c0
AlCap23 <julius.martensen@gmail.com>
2021-07-27 20:19:22 +0200

    Further updates

--- scripts/optimizing_code.jl
+++ scripts/optimizing_code.jl
@@ -1,4 +1,3 @@
-using LinearAlgebra: length
 using LinearAlgebra
 
 g(x, y) = x + sin(x)*y^2
@@ -22,9 +21,31 @@ z = randn(Float32, 10);
 
 @time g(z,y)
 
-# Benchmark further
+## Memory Layout
 using BenchmarkTools
 
+function sum_by_row(x::AbstractMatrix)
+    res = zero(eltype(x))
+    for i in 1:size(x, 1)
+        res += sum(x[i,:])
+    end
+    return res
+end
+
+function sum_by_col(x::AbstractMatrix)
+    res = zero(eltype(x))
+    for i in 1:size(x, 2)
+        res += sum(x[:,i])
+    end
+    return res
+end
+
+x = randn(Float32, 10_000, 10_000);
+
+@btime sum_by_row($x) # 713.33 ms (20000 allocations: 382.23 MiB)
+@btime sum_by_col($x) # 99.842 ms (20000 allocations: 382.23 MiB)
+
+## SIMD
 function sum_vectors!(x, y, z)
     n = length(x)
     for i in 1:n
@@ -43,7 +64,20 @@ a = zeros(Float32, 100_000);
 b = randn(Float32, 100_000);
 c = randn(Float32, 100_000);
 
-@btime sum_vectors!($a,$b,$c)
-@btime sum_vectors_simd!($a,$b,$c)
+@btime sum_vectors!($a,$b,$c) # 79.932 μs (0 allocations: 0 bytes)
+@btime sum_vectors_simd!($a,$b,$c) # 18.960 μs (0 allocations: 0 bytes)
 
+## For the interested
 @code_llvm sum_vectors_simd!(a,b,c)
+
+## Multithreading
+using Base.Threads
+
+function sum_vectors_threading!(x, y, z)
+    n = length(x)
+    @inbounds Threads.@threads for i in 1:n
+        x[i] = y[i] + z[i]
+    end
+end
+
+@btime sum_vectors_threading!($a,$b,$c) # 132.019 μs (6 allocations: 560 bytes)
